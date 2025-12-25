bac0eb259fd9ecdd9597240a6a8188f5a0315e0d
Winifred Young <winifred.young00@gmail.com>
2025-12-25 15:29:36 +0100

    Remove duplicate hardcoded CORS config in WebConfig

--- src/main/java/com/capstone/huddle/config/WebConfig.java
+++ src/main/java/com/capstone/huddle/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.capstone.huddle.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,10 +31,5 @@ public class WebConfig implements WebMvcConfigurer {
         log.info("Serving uploaded files from: file:{}{}", absolutePath, File.separator);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/huddle/uploads/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET");
-    }
+    // CORS is handled by CorsConfig.java - removed duplicate config here
 }
